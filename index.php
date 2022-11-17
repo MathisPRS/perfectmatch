2f8a2f3f5a7eeeaea19a04cb047f511fcd1f90e0
mathis <mathis.reclus@gmail.com>
2022-11-17 23:24:25 +0100

    ajout d'info page index

--- index.php
+++ index.php
@@ -20,20 +20,36 @@ include 'header.php';
                 <img class="image1" src="/img/men_and_wife_kissing_0a76704d6f.jpeg">
             </div>
             <p>The Perfect Match, agence de Wedding et Event Planner <br> vous accompagne dans la création de vos plus beaux <br>souvenirs, sur Paris et Bordeaux.</p>
-            <img>
-        </section class="index_2">
-        <div>
-                <span>Vous allez</span>
-                <span>vous marier ?</span>
-                <span>La demande tant attendue est arrivée ?</span>
-                <span>Félicitations !</span>
-        </div>
-                <p>Si vous êtes ici, c’est sûrement que vous vous
-                    <br>posez quelques questions à propos de l’organisation de votre grand jour.
-                    <br><strong>Alors vous vous trouvez au bon endroit.</strong>
-                </p>
+            <img class="image2" src="/img/wedding_men_and_wife_68b55d5605.jpeg">
+        </section>
+
+        <section class="index_2">
+            <div class="title_index_2">
+                    <span>Vous allez</span>
+                    <span><strong>vous marier ?</strong></span>
+                    <br>
+                    <span>La demande tant attendue est arrivée ?</span>
+                    <br>
+                    <span>Félicitations !</span>
+            </div>
+            <p>Si vous êtes ici, c’est sûrement que vous vous
+                <br>posez quelques questions à propos de l’organisation de votre grand jour.
+                <br><strong>Alors vous vous trouvez au bon endroit.</strong>
+            </p>
+        </section>
+
+        <section class="index_3">
+            <h2 class="title_index_3">
+                <span>Une agence d'organisation</span><br>
+                <span>d'<strong>évènements</strong>privé</span>
+            </h2>
+            <p>Basée entre Paris et Bordeaux, nous vous proposons de choisir l'évènement <br>
+            de votre choix dans la région qui vous fait envie, afin de satisfaire tous vos <br>
+            rêves.
+            </p>    
+            <button class="button_index_3">EN SAVOIR PLUS</button>    
+        </section>
 
         <section>
-
-        </section class="index_2">
+        </section>
     </body>
