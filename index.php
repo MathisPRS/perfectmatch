0696f2a443bd883aa5475989457b2ca28cd57586
mathis <mathis.reclus@gmail.com>
2022-11-20 22:15:31 +0100

    ajout d'info page index

--- index.php
+++ index.php
@@ -34,23 +34,26 @@ include 'header.php';
             <br>posez quelques questions à propos de l’organisation de votre<br>grand jour.
             <br><strong>Alors vous vous trouvez au bon endroit.</strong>
         </p>
+        <img src="" alt="">
     </section>
 
     <section class="index_3">
-        <h2 class="title_index_3">
-            <span>Une agence d'organisation</span><br>
-            <span>d'<strong>évènements</strong> privé</span>
-        </h2>
-        <p>Basée entre Paris et Bordeaux, nous vous proposons de choisir l'évènement <br>
-        de votre choix dans la région qui vous fait envie, afin de satisfaire tous vos <br>
-        rêves.
-        </p>    
-        <button class="button_index_3">EN SAVOIR PLUS</button>    
+        <div>
+            <h2 class="title_index_3">
+                <span>Une agence d'organisation</span><br>
+                <span>d'<strong>évènements</strong> privé</span>
+            </h2>
+            <p>Basée entre Paris et Bordeaux, nous vous proposons de choisir l'évènement <br>
+            de votre choix dans la région qui vous fait envie, afin de satisfaire tous vos <br>
+            rêves.
+            </p>    
+            <button class="button_index_3">EN SAVOIR PLUS</button>
+        </div>
     </section>
 
     <section>
         <div>
-            <img>
+            <img src="" alt="">
             <p>Pour contenter chaque futur marié, nous nous adaptons à tout type de demande et de budget.<strong> A vous de choisir la formule qui vous correspond le mieux !<strong></p>
         </div>
         
@@ -62,33 +65,59 @@ include 'header.php';
     </section>
     
     <section>
-
-    </section>
-    
-    
-    
-    
-    <section>
-            <p>ARTICLES</p>
-            <span>Besoins d'</span><span><strong>inspiration</strong> ?</span>
-            <span>Envie d'un peu de lecture ?</span>
+        <div>
             <div>
-                <div>
-                    <img>
-                    <h4 href>Gérer sa liste d'invités</h4>
-                    <p>Ça y est, un des jours que vous attendiez le plus est arrivé, la demande en mari...</p>
-                </div>
-                <div>
-                    <img>
-                    <h4 href>Comment choisir sa Wedding Planner?</h4>
-                    <p>Vous vous lancez dans la grande aventure du mariage et vous ne savez pas par où ...</p>
-                </div>
-                <div>
-                    <img>
-                    <h4 href>Organiser un mariage à petit budget</h4>
-                    <p>Organiser un mariage avec un petit budget c’est possible ? Un beau mariage n’es...</p>
-                </div>    
+                <img src="" alt="">
+                <h3>Organisation Complète</h3>
+                <p>Une organisation clés en mains, il ne vous reste plus qu'à pro-fi-ter !</p>
+                <button>DÉCOUVRIR</button>
             </div>
-            <button>VOIR PLUS</button>
+
+            <div>
+                <img src="" alt="">
+                <h3>Organisation Partielle</h3>
+                <p>Une aide précieuse pour vous épauler dans l'organisation de votre mariage.</p>
+                <button>DÉCOUVRIR</button>
+            </div>
+            <div>
+                <img src="" alt="">
+                <h3>Organisation Complète</h3>
+                <p>Une organisation clés en mains, il ne vous reste plus qu'à pro-fi-ter !</p>
+                <button>DÉCOUVRIR</button>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div>
+            <p>ÉVENEMENTS</p>
+            <span>Vous souhaitez fêter un<br></span>
+            <span>évènement qui vous<br></span>
+            <span><strong>tient à coeur ?</strong></span>
+            <p><strong>The Perfect Match s'engage à ne faire que du sur-mesure !</strong><br>Parce que votre évènement est unique, en passant par la demande en mariage, l'organisation de vos fiançailles, une baby<br>shower, une fête religieuse, ou encore votre anniversaire, nous organisons tout de A à Z.</p>
+            <button>DÉCOUVRIR</button>
+        </div>
+    </section>
+    <section>
+        <p>ARTICLES</p>
+        <span>Besoins d'</span><span><strong>inspiration</strong> ?</span>
+        <span>Envie d'un peu de lecture ?</span>
+        <div>
+            <div>
+                <img>
+                <h4 href>Gérer sa liste d'invités</h4>
+                <p>Ça y est, un des jours que vous attendiez le plus est arrivé, la demande en mari...</p>
+            </div>
+            <div>
+                <img>
+                <h4 href>Comment choisir sa Wedding Planner?</h4>
+                <p>Vous vous lancez dans la grande aventure du mariage et vous ne savez pas par où ...</p>
+            </div>
+            <div>
+                <img>
+                <h4 href>Organiser un mariage à petit budget</h4>
+                <p>Organiser un mariage avec un petit budget c’est possible ? Un beau mariage n’es...</p>
+            </div>    
+        </div>
+        <button class="button_index_3">VOIR PLUS</button>
     </section>
 </body>
